32092fedf27a509d7a5a29defc536b7162fe841d
Vitali <witosmartyn@mail.ru>
2018-07-17 01:09:52 +0300

    replace letter array to list list(String or Character) (variaty in controller commented)

--- src/main/java/com/witosmartyn/app/board/BoardController.java
+++ src/main/java/com/witosmartyn/app/board/BoardController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -23,6 +26,8 @@ public class BoardController {
     private final Logger log = LoggerFactory.getLogger(BoardController.class);
     private IBoardService boardService;
     private ParamsValidator paramsValidator;
+    private List<Character> alphabet = Constants.ALPHABET.chars().mapToObj(c-> (char)c).collect(Collectors.toList());
+    private List<String> alphabetStr = Arrays.asList(new String[]{"JAVA", "PHP", "C++", "JS", "Python"});
 
     @Resource
     public void setParamsValidator(ParamsValidator paramsValidator) {
@@ -38,9 +43,13 @@ public class BoardController {
      * @return The  instance object that wraps the parameters
      */
     @ModelAttribute(value = "params")
-    public Params newParams(){
-        return new Params();
+    public Params newParams() {
+        final Params params = new Params();
+        params.setSize(8);
+        return params;
+
     }
+
     /**
      * @return View page
      */
@@ -49,23 +58,26 @@ public class BoardController {
         return Pages.FORM;
     }
 
-    /** @param  params
-     *        The object that wraps the parameters
-     * Retrieves Query Parameters and
-     * Passes its to the BoardService
-     * and receive Board with cells
-     * Puts result to model
+    /**
+     * @param params The object that wraps the parameters
+     *               Retrieves Query Parameters and
+     *               Passes its to the BoardService
+     *               and receive Board with cells
+     *               Puts result to model
      * @return view page
      */
     @PostMapping("/")
     public String postSize(@ModelAttribute("params") Params params, BindingResult bindRes, Model model) {
         log.info("size:" + params.getSize());
 
-        if(bindRes.hasErrors() || !paramsValidator.isValid(params)){
-            model.addAttribute("errorMessage",true);
+        if (bindRes.hasErrors() || !paramsValidator.isValid(params)) {
+            model.addAttribute("errorMessage", true);
             return Pages.FORM;
         }
-        model.addAttribute("alphabet", Constants.ALPHABET.toCharArray());
+
+//        model.addAttribute("alphabet", alphabetStr);
+        model.addAttribute("alphabet", alphabet);
+
         model.addAttribute(Constants.BOARD, boardService.generateBoard(params));
         return Pages.BOARD;
     }
--- src/test/java/com/witosmartyn/app/board/BoardServiceImplTest.java
+++ src/test/java/com/witosmartyn/app/board/BoardServiceImplTest.java
@@ -7,7 +7,6 @@ import com.witosmartyn.app.board.models.Params;
 import com.witosmartyn.app.config.Color;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -43,7 +42,7 @@ public class BoardServiceImplTest {
         assertTrue(cells.get(0).size()==2);
         assertNotNull(cells.get(0).get(0).getColor());
         assertNotNull(cells.get(0).get(1).getColor());
-        assertTrue(cells.get(0).get(0).getColor().equals(Color.WHITE));
-        assertTrue(cells.get(0).get(1).getColor().equals(Color.BLACK));
+        assertTrue(cells.get(0).get(0).getColor().equals(Color.BLACK));
+        assertTrue(cells.get(0).get(1).getColor().equals(Color.WHITE));
     }
 }
